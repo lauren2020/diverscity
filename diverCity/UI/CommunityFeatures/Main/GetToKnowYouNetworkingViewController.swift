7b5c2fd9299c7837dadf65e9bb9f77c2e168a297
Lauren Shultz <lshultz2020@gmail.com>
2019-03-08 09:50:52 -0600

    Adding get to know you view controller

---
+++ diverCity/UI/CommunityFeatures/Main/GetToKnowYouNetworkingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GetToKnowYouNetworkingViewController.swift
+//  diverCity
+//
+//  Created by Lauren Shultz on 3/8/19.
+//  Copyright © 2019 Lauren Shultz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GetToKNowYouNetworkingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
